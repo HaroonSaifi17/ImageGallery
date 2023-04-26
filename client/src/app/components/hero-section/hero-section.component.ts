dfcab13244769b9384963415b31bf6528daa794d
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-04-27 01:53:36 +0530

    Some twicks

--- client/src/app/components/hero-section/hero-section.component.ts
+++ client/src/app/components/hero-section/hero-section.component.ts
@@ -9,11 +9,31 @@ import { ImgDataService } from 'src/app/services/img-data.service'
 })
 export class HeroSectionComponent implements OnInit {
   data: DataInterface[] = []
+  copyData: DataInterface[] = []
+  openCheck:boolean= false
+  currentImg:string=''
   constructor(private imgData: ImgDataService) { }
 
   ngOnInit(): void {
+    this.getdata()
+  }
+  getdata():void{
     this.imgData.getData().subscribe((data: DataInterface[]) => {
       this.data = data
+      this.copyData = data
     })
+  }
+  search(value:string):void{
+     let searchdata:DataInterface[]=[]
+     this.copyData.forEach((element:DataInterface) => {
+      if(element.description.toLowerCase().startsWith(value.toLowerCase())){
+        searchdata.push(element)
+      } 
+     });
+    this.data=searchdata
+  }
+  openLightBox(i:number){
+    this.openCheck= true
+    this.currentImg=this.data[i].url
   }
 }
