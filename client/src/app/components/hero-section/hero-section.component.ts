3afc6c3bc25333fe9bb529fb9bd30b3e87424d4f
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-04-27 18:31:08 +0530

    adding some functionallity for lightbox

--- client/src/app/components/hero-section/hero-section.component.ts
+++ client/src/app/components/hero-section/hero-section.component.ts
@@ -10,30 +10,49 @@ import { ImgDataService } from 'src/app/services/img-data.service'
 export class HeroSectionComponent implements OnInit {
   data: DataInterface[] = []
   copyData: DataInterface[] = []
-  openCheck:boolean= false
-  currentImg:string=''
-  constructor(private imgData: ImgDataService) { }
+  openCheck: boolean = false
+  leftCheck: boolean = true
+  rightCheck: boolean = true
+  Img: string = ''
+  Description: string = ''
+  ii: number = 0
+  constructor(private imgData: ImgDataService) {}
 
   ngOnInit(): void {
     this.getdata()
   }
-  getdata():void{
+  getdata(): void {
     this.imgData.getData().subscribe((data: DataInterface[]) => {
       this.data = data
       this.copyData = data
     })
   }
-  search(value:string):void{
-     let searchdata:DataInterface[]=[]
-     this.copyData.forEach((element:DataInterface) => {
-      if(element.description.toLowerCase().startsWith(value.toLowerCase())){
+  search(value: string): void {
+    let searchdata: DataInterface[] = []
+    this.copyData.forEach((element: DataInterface) => {
+      if (element.description.toLowerCase().startsWith(value.toLowerCase())) {
         searchdata.push(element)
-      } 
-     });
-    this.data=searchdata
+      }
+    })
+    this.data = searchdata
   }
-  openLightBox(i:number){
-    this.openCheck= true
-    this.currentImg=this.data[i].url
+  openLightBox(i: number) {
+    if (i == 0) {
+      this.leftCheck = false
+    } else {
+      this.leftCheck = true
+    }
+    if (i + 1 == this.data.length) {
+      this.rightCheck = false
+    } else {
+      this.rightCheck = true
+    }
+    this.openCheck = true
+    this.Img = this.data[i].url
+    this.Description = this.data[i].description
+    this.ii = i
+  }
+  close(): void {
+    this.openCheck = false
   }
 }
